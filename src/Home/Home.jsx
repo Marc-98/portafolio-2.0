13deae746c0b8cf9f075073345bfb033ee99b567
Marc Meza <105956101+Marc-98@users.noreply.github.com>
2025-05-16 02:04:53 -0500

    Update Home.jsx

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,7 +2,7 @@ import "./Home.css"
 import Hero from '../Components/Hero/Hero'
 import About from '../Components/About/About'
 import Skills from "../Components/Skills/Skills"
-import Portfolio from "../Components/portfolio/portfolio"
+import Portfolio from "../Components/Portfolio/Portfolio"
 import ContactForm from "../Components/ContactForm/ContactForm"
 import Footer from "../Components/Footer/Footer"
 const Home = () => {
